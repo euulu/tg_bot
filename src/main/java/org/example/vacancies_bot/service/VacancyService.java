7a8bde45e58a311e910cd9d9686a85749e8dba72
euulu <anegamega@gmail.com>
2023-12-19 18:00:12 +0200

    Add vacancy service

---
+++ src/main/java/org/example/vacancies_bot/service/VacancyService.java
@@ -0,0 +1,36 @@
+package org.example.vacancies_bot.service;
+
+import jakarta.annotation.PostConstruct;
+import org.example.vacancies_bot.dto.VacancyDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class VacancyService {
+    @Autowired
+    private VacanciesReaderService vacanciesReaderService;
+
+    private final Map<String, VacancyDto> vacancies = new HashMap<>();
+
+    @PostConstruct
+    public void init() {
+        List<VacancyDto> list = vacanciesReaderService.getVacanciesFromFile("vacancies.csv");
+        for (VacancyDto vacancy: list) {
+            vacancies.put(vacancy.getId(), vacancy);
+        }
+    }
+
+    public List<VacancyDto> getJuniorVacancies() {
+        return vacancies.values().stream()
+                .filter(v -> v.getTitle().toLowerCase().contains("junior"))
+                .toList();
+    }
+
+    public VacancyDto get(String id) {
+        return vacancies.get(id);
+    }
+}
