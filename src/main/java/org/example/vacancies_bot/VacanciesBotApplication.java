6d2a1d90344f52d4712c8ff5c535e9d44a62dcb6
euulu <anegamega@gmail.com>
2023-12-19 11:05:58 +0200

    Init

---
+++ src/main/java/org/example/vacancies_bot/VacanciesBotApplication.java
@@ -0,0 +1,13 @@
+package org.example.vacancies_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacanciesBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacanciesBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/vacancies_bot/VacanciesBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.vacancies_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacanciesBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
