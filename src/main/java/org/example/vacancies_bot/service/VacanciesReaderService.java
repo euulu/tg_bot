411de36da338859d4765507e4a9357be01e00e50
euulu <anegamega@gmail.com>
2023-12-19 17:58:20 +0200

    Add reader from file service

---
+++ src/main/java/org/example/vacancies_bot/service/VacanciesReaderService.java
@@ -0,0 +1,31 @@
+package org.example.vacancies_bot.service;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import org.example.vacancies_bot.dto.VacancyDto;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@Service
+public class VacanciesReaderService {
+    public List<VacancyDto> getVacanciesFromFile(String fileName) {
+        Resource resource = new ClassPathResource(fileName);
+
+        try (InputStreamReader inputStreamReader = new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8)) {
+            CsvToBean<VacancyDto> csvToBean = new CsvToBeanBuilder<VacancyDto>(inputStreamReader)
+                    .withType(VacancyDto.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+
+            return csvToBean.parse();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from file " + fileName, e);
+        }
+    }
+}
