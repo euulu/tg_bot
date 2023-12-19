123322f713f757921c379b32526358408f635152
euulu <anegamega@gmail.com>
2023-12-19 17:57:06 +0200

    Add Vacancy dto

---
+++ src/main/java/org/example/vacancies_bot/dto/VacancyDto.java
@@ -0,0 +1,36 @@
+package org.example.vacancies_bot.dto;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class VacancyDto {
+    @CsvBindByName(column = "Id")
+    private String id;
+    @CsvBindByName(column = "Title")
+    private String title;
+    @CsvBindByName(column = "Short description")
+    private String shortDescription;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+}
